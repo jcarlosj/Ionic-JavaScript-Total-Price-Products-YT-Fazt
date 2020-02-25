a17cfd52dcab99a7de84b3d8430c3fbf56920fc3
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-25 10:31:45 -0500

    [+] index: Show products

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ const /** Obtengo los elementos del DOM */
     $productName  = document .querySelector( '#product-name' ),
     $productPrice = document .querySelector( '#product-price' ),
     $btnSave      = document .querySelector( '#btn-save' ),
-    $btnClear     = document .querySelector( '#btn-clear' );
+    $btnClear     = document .querySelector( '#btn-clear' ),
+    $productList  = document .querySelector( '#product-list' );
 
 /** Eventos */
 $btnSave .addEventListener( 'click', () => {
@@ -11,4 +12,18 @@ $btnSave .addEventListener( 'click', () => {
         price: Number( $productPrice .value )
     }
     console .log( 'Producto', product );
+    createNewProduct( product );
+
 });
+
+/** Crea estructura del DOM usando Ionic para un nuevo producto a mostrar */
+const createNewProduct = ( product ) => {
+    const /** Crea elementos para el DOM */
+        $ionCard = document .createElement( 'ion-card' ),
+        $ionCardContent = document .createElement( 'ion-card-content' );
+
+    $ionCardContent .textContent = `${ product .name } $ ${ product .price }`;   // Insertamos texto al elemento
+    $ionCard .appendChild( $ionCardContent );       // Insertamos elemento dentro de otro elemento
+    $productList .appendChild( $ionCard );          // Inserta elemento al DOM
+
+}
